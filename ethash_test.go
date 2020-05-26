c686fb229d007c942f4bb2d30cb15cf2adf7a110
Codegita <codegita@valorbit.com>
2020-05-26 16:13:25 -0700

    Update vendor path

--- ethash_test.go
+++ ethash_test.go
@@ -27,8 +27,8 @@ import (
 	"sync"
 	"testing"
 
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/valorbit/go-ethereum/common"
+	"github.com/valorbit/go-ethereum/crypto"
 )
 
 func init() {
